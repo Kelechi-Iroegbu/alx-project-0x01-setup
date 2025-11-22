a0f200c5947b8c848674b3d3f4cee52847a81ae6
kelly <kiroegbu@gmail.com>
2025-11-22 20:49:59 +0100

    updates

---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-900 text-white p-4 text-center">
+      <p>© 2025 ALX Project</p>
+    </footer>
+  );
+};
+
+export default Footer;
+g
