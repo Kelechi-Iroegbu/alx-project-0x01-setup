ac6c66ed1c61dd6967f152f2631f669e26e93241
Kelly <74309549+Kelechi-Iroegbu@users.noreply.github.com>
2026-02-12 15:46:31 +0100

    Update index.tsx

--- alx-project-0x01/pages/index.tsx
+++ alx-project-0x01/pages/index.tsx
@@ -23,3 +23,4 @@ const Home: React.FC = () => {
 export async function getStaticProps() {", "https://jsonplaceholder.typicode.com/users", "export default Users;
 export default Home;
 export async function getStaticProps() {", "https://jsonplaceholder.typicode.com/users", "export default Users
+export async function getStaticProps() {", "https://jsonplaceholder.typicode.com/users", "export default Users;"
